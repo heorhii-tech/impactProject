9bff2ac32656dd7c53e6df7bedc215956862c4ed
Hassan Shaif <hassanshaif8@gmail.com>
2023-06-13 10:42:06 +0200

    login page  get funct and //post func basic style for login page

--- controller/userController.js
+++ controller/userController.js
@@ -2,8 +2,37 @@ const getAuthPage = (req,res)=>{
     res.render('startPage')
 }
 const getLoginPage = (req,res)=>{
-res.send('hello')
+res.render('logInPage')
 }
+
+//const postLoginPage = (req,res)=>{
+//   // Check if this user is already in the DB.
+//    let existedUser = await userModel.findOne({email: req.body.email});
+//     if(!existedUser) {
+//        res.render('signup', {
+//            error: "user is not exist. So signup first please!",
+//            success: ""
+//        })
+//     } else {
+//        let isCorrectPass = bcrypt.compareSync(req.body.password, existedUser.password);
+//        if(!isCorrectPass) {
+//            res.render('signin', {
+//                error: "user password is not correct!",
+//                success: ""
+//            })
+//        } else {
+//            let infoForToken = {
+//                id: existedUser._id,
+//                name: existedUser.userName,
+//                email: existedUser.email
+//           }
+//                  let userToken = jwt.sign({infoForToken}, process.env.JWT_TEXT);
+//            res.cookie("userToken", userToken, {httpOnly: true});
+//            res.redirect('/')
+//        }
+//    }
+//}
+
 const getSignPage = (req,res)=>{
     res.send('hello')
 }
@@ -14,6 +43,7 @@ const getQuestionPage = (req,res)=>{
 module.exports= {
     getAuthPage,
     getLoginPage,
+//    postLoginPage,
     getSignPage,
     getQuestionPage
 }
